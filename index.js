ff20a34cb2cf9b3cff568639f11748e162908432
André Fonseca <andre.fonseca@rethink.dev>
2023-04-10 14:02:27 -0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const Joi = require("joi");
+const logger = require("./middleware/logger");
+const auth = require("./auth");
+const helmet = require("helmet");
+const morgan = require("morgan");
+const config = require("config");
+const startupDebugger = require("debug")("app:startup");
+const dbDebugger = require("debug")("app:db");
+const courses = require("./routes/courses");
+const home = require("./routes/home");
+const app = express();
+
+app.set("view engine", "pug");
+app.set("views", "./views");
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+app.use(helmet());
+app.use("/api/courses", courses);
+app.use("/", home);
+
+// Configuration
+console.log("Application Name: " + config.get("name"));
+console.log("Mail Server: " + config.get("mail.host"));
+console.log("Mail Password: " + config.get("mail.password"));
+
+if (app.get("env") === "development") {
+  app.use(morgan("tiny"));
+  startupDebugger("Morgan enabled...");
+}
+
+// Db work...
+dbDebugger("Connected to the database...");
+
+app.use(logger);
+app.use(auth);
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Listening on port ${port}...`));
